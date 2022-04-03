d66a02eee2feb8990609f78cceffe0332f1233d9
pizzzza-planet <pizzzzzzzza.planet@gmail.com>
2022-04-03 12:30:37 +0900

    laravel breezeのインストール

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//Sample
 Route::get('/sample', [\App\Http\Controllers\Sample\IndexController::class,'show']);
 Route::get('/sample/{id}', [\App\Http\Controllers\Sample\IndexController::class,'showId']);
+
+//Tweet
 Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class)->name('tweet.index');
 Route::post('/tweet/create', \App\Http\Controllers\Tweet\CreateController::class)->name('tweet.create');
 Route::get('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\IndexController::class)
@@ -26,3 +29,9 @@ Route::get('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\IndexCo
 Route::put('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\PutController::class)
 ->name('tweet.update.put')->where('tweetId', '[0-9]+');
 Route::delete('/tweet/delete/{tweetId}', \App\Http\Controllers\Tweet\DeleteController::class)->name('tweet.delete');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
